0cb1402b197182ec7f93a8eca8fc506d6fb08ad4
jose m. delgado <jdelgado2002@gmail.com>
2022-03-08 09:07:24 -0600

    displaying, and creating projects

---
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/wwwroot/view-resources/Views/Projects/_EditModal.js
@@ -0,0 +1,39 @@
+﻿(function ($) {
+    var _projectService = abp.services.app.project,
+        l = abp.localization.getSource('todo'),
+        _$modal = $('#ProjectEditModal'),
+        _$form = _$modal.find('form');
+
+    function save() {
+        if (!_$form.valid()) {
+            return;
+        }
+
+        var project = _$form.serializeFormToObject();
+
+        abp.ui.setBusy(_$form);
+        _projectService.update(project).done(function () {
+            _$modal.modal('hide');
+            abp.notify.info(l('SavedSuccessfully'));
+            abp.event.trigger('project.edited', project);
+        }).always(function () {
+            abp.ui.clearBusy(_$form);
+        });
+    }
+
+    _$form.closest('div.modal-content').find(".save-button").click(function (e) {
+        e.preventDefault();
+        save();
+    });
+
+    _$form.find('input').on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            save();
+        }
+    });
+
+    _$modal.on('shown.bs.modal', function () {
+        _$form.find('input[type=text]:first').focus();
+    });
+})(jQuery);
