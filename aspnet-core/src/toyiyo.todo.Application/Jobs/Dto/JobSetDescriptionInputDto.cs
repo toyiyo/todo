3d880b9c809dc91e85f20d4eeca6f3b5cda97ccb
jose m. delgado <jdelgado2002@gmail.com>
2022-07-16 20:44:58 -0500

    enabling markdown editing using tui.editor/

--- aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetDescriptionInputDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetDescriptionInputDto.cs
@@ -6,7 +6,6 @@ namespace toyiyo.todo.Jobs
 {
     public class JobSetDescriptionInputDto : EntityDto<Guid>
     {
-        [Required]
         [StringLength(Job.MaxDescriptionLength)]
         public string Description { get; set; }
     }
--- aspnet-core/src/toyiyo.todo.Web.Mvc/wwwroot/view-resources/Views/Jobs/_EditModal.js
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/wwwroot/view-resources/Views/Jobs/_EditModal.js
@@ -1,4 +1,15 @@
 ﻿(function ($) {
+    
+    const Editor = toastui.Editor;
+    
+    const editor = new Editor({
+        el: document.querySelector('#Description'),
+        initialValue: $('#descriptionFromServer').val(),
+        height: '400px',
+        initialEditType: 'wysiwyg',
+        previewStyle: 'vertical'
+    });
+
     var _jobService = abp.services.app.job,
         l = abp.localization.getSource('todo'),
         _$modal = $('#JobEditModal'),
@@ -9,7 +20,11 @@
             return;
         }
 
+        //serialization works for input types, the description is a div so that we can have markdown functionality.  
+        //We need to get the div contents and manually add them to the job object
         var job = _$form.serializeFormToObject();
+        job.description = editor.getMarkdown();
+        //editor.getMarkdown();
 
         abp.ui.setBusy(_$form);
         //calling multiple services in parallel will fetch data from the DB in parallel.
