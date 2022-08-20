606e0d3e04c4b1675364f09aec9780cb310695f5
jose m. delgado <jdelgado2002@gmail.com>
2022-08-20 17:40:17 -0500

    adding datatypes

--- aspnet-core/src/toyiyo.todo.Web.Mvc/Models/Account/RegisterCompanyAdminViewModel.cs
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/Models/Account/RegisterCompanyAdminViewModel.cs
@@ -14,20 +14,24 @@ namespace toyiyo.todo.Web.Models.Account
         [Required]
         [StringLength(AbpTenantBase.MaxTenancyNameLength)]
         [RegularExpression(AbpTenantBase.TenancyNameRegex)]
+        [DataType(DataType.Text)]
         public string TenancyName { get; set; }
 
         [Required]
         [StringLength(AbpTenantBase.MaxNameLength)]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
         [Required]
         [EmailAddress]
         [StringLength(AbpUserBase.MaxEmailAddressLength)]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
         [Required]
         [StringLength(AbpUserBase.MaxPlainPasswordLength)]
         [DisableAuditing]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public bool IsExternalLogin { get; set; }
