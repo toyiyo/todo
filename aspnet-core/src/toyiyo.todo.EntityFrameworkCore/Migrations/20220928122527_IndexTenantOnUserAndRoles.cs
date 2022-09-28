9ddc8da7862df56e340d65797532949489297f2d
Jose M Delgado <jdelgado2002@gmail.com>
2022-09-28 07:53:36 -0500

    adding indexes on tenant to role and user and on job status to jobs

---
+++ aspnet-core/src/toyiyo.todo.EntityFrameworkCore/Migrations/20220928122527_IndexTenantOnUserAndRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace toyiyo.todo.Migrations
+{
+    public partial class IndexTenantOnUserAndRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpUsers_TenantId",
+                table: "AbpUsers",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpRoles_TenantId",
+                table: "AbpRoles",
+                column: "TenantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpUsers_TenantId",
+                table: "AbpUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbpRoles_TenantId",
+                table: "AbpRoles");
+        }
+    }
+}
---
+++ aspnet-core/src/toyiyo.todo.EntityFrameworkCore/Migrations/20220928124611_IndexJobStatusOnJobs.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace toyiyo.todo.Migrations
+{
+    public partial class IndexJobStatusOnJobs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_JobStatus",
+                table: "Jobs",
+                column: "JobStatus");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_JobStatus",
+                table: "Jobs");
+        }
+    }
+}
