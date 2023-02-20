5b24bd7b0208daaeb4dd16806a570b510a5f5de9
Jose M Delgado <jdelgado2002@gmail.com>
2023-02-19 19:23:53 -0600

    passing job stats to the view

--- aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/HomeController.cs
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Abp.AspNetCore.Mvc.Authorization;
 using toyiyo.todo.Controllers;
+using toyiyo.todo.Jobs;
+using toyiyo.todo.Web.Models.Jobs;
+using System.Threading.Tasks;
 
 namespace toyiyo.todo.Web.Controllers
 {
     [AbpMvcAuthorize]
     public class HomeController : todoControllerBase
     {
-        public ActionResult Index()
+        private IJobAppService _jobAppService;
+
+        public HomeController(IJobAppService jobAppService)
         {
-            return View();
+            _jobAppService = jobAppService;
+        }
+
+        public async Task<ActionResult> IndexAsync()
+        {
+            var jobStats = await _jobAppService.GetJobStats(new GetAllJobsInput(){ MaxResultCount = int.MaxValue });
+
+            return View(jobStats);
         }
     }
 }
