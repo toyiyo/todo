d3a7cd59e79989302d871b14e44ee2ffd51a9df3
Jose M Delgado <jdelgado2002@gmail.com>
2023-02-20 11:08:36 -0600

    serialize the jobs completed per month to json and set on viewbag to get access via html

--- aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/HomeController.cs
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/HomeController.cs
@@ -2,8 +2,8 @@
 using Abp.AspNetCore.Mvc.Authorization;
 using toyiyo.todo.Controllers;
 using toyiyo.todo.Jobs;
-using toyiyo.todo.Web.Models.Jobs;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace toyiyo.todo.Web.Controllers
 {
@@ -20,7 +20,8 @@ namespace toyiyo.todo.Web.Controllers
         public async Task<ActionResult> IndexAsync()
         {
             var jobStats = await _jobAppService.GetJobStats(new GetAllJobsInput(){ MaxResultCount = int.MaxValue });
-
+            var jsonTotalCompletedJobsPerMonth= JsonConvert.SerializeObject(jobStats.TotalCompletedJobsPerMonth);
+            ViewBag.TotalCompletedJobsPerMonth = jsonTotalCompletedJobsPerMonth;
             return View(jobStats);
         }
     }
