9de89041222b108ca7273481a09afde91f25556c
Jose M Delgado <jdelgado2002@gmail.com>
2024-01-23 15:16:54 -0600

    adding subscription info at the tenant level

--- aspnet-core/src/toyiyo.todo.Core/MultiTenancy/Tenant.cs
+++ aspnet-core/src/toyiyo.todo.Core/MultiTenancy/Tenant.cs
@@ -6,8 +6,12 @@ namespace toyiyo.todo.MultiTenancy
 {
     public class Tenant : AbpTenant<User>
     {
-        [DefaultValue("false")]
+        [DefaultValue("true")]
         public bool AllowsSelfRegistration { get; set; }
+        //todo: at the moment, we are getting this info from stripe, but we should store it in the database via webhooks.
+        //this should belong in the subscription class, but we are not using it yet.
+        public string ExternalSubscriptionId { get; protected set; }
+        public int SubscriptionSeats { get; protected set; }
         public Tenant()
         {            
         }
