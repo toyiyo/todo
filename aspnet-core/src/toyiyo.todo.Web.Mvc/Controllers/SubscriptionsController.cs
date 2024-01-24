307d73bbae704b8c729b20808d61ebd9b7175042
Jose M Delgado <jdelgado2002@gmail.com>
2024-01-24 16:39:08 -0600

    passing email and client reference (tenant) to stripe, migration for tenant table to support subscription info

--- aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/SubscriptionsController.cs
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/SubscriptionsController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Threading.Tasks;
 using Abp.AspNetCore.Mvc.Authorization;
+using Abp.Runtime.Session;
 using Microsoft.AspNetCore.Mvc;
 using toyiyo.todo.application.subscriptions;
+using toyiyo.todo.Authorization.Users;
 using toyiyo.todo.Controllers;
+using toyiyo.todo.Users;
 
 namespace toyiyo.todo.Web.Controllers
 {
@@ -11,14 +14,23 @@ namespace toyiyo.todo.Web.Controllers
     public class SubscriptionsController : todoControllerBase
     {
         private readonly ISubscriptionsService _subscriptionsService;
+        private readonly UserManager _userManager;
 
-        public SubscriptionsController(ISubscriptionsService subscriptionsService)
+        public SubscriptionsController(ISubscriptionsService subscriptionsService, UserManager userManager)
         {
             _subscriptionsService = subscriptionsService;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
+            // Get the subscription for the current tenant
             var subscriptionDto = await _subscriptionsService.GetSubscriptionForTenant();
+
+            //get the current
+            var user = await _userManager.FindByIdAsync(AbpSession.GetUserId().ToString());
+            //pass email and userid to view
+            ViewBag.Email = user.EmailAddress;
+            ViewBag.UserId = AbpSession.GetUserId();
 
             // Check if subscriptionDto is not null and has a valid ProductId and PlanId
             if (subscriptionDto != null && !string.IsNullOrEmpty(subscriptionDto.ProductId) && !string.IsNullOrEmpty(subscriptionDto.PlanId))
@@ -33,6 +45,7 @@ namespace toyiyo.todo.Web.Controllers
                 subscriptionDto.PortalSessionUrl = portalSession.Url;
                 subscriptionDto.ProductName = productDto.Name;
             }
+
             return View(subscriptionDto);
         }
     }
