bf8b68fcbab3c6b7648c0395be82b21da10db335
Jose M Delgado <jdelgado2002@gmail.com>
2024-01-24 15:17:53 -0600

    annotating public DTOs

--- aspnet-core/src/toyiyo.todo.Application/Subscriptions/Dto/ProductDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Subscriptions/Dto/ProductDto.cs
@@ -3,13 +3,31 @@ using Stripe;
 
 namespace toyiyo.todo.application.subscriptions
 {
+    /// <summary>
+    /// Stripe's product instance representation, this is what you are selling.
+    /// </summary>
     [AutoMap(typeof(Product))]
     public class ProductDto
     {
+        /// <summary>
+        /// Gets or sets the ID of the product.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// Stripe's product name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Livemode determines if the session is in live mode or test mode.
+        /// </summary>
         public string Livemode { get; set; }
+        /// <summary>
+        /// The products description
+        /// </summary>
         public string Description { get; set; }
+        /// <summary>
+        /// Is the product active
+        /// </summary>
         public bool Active { get; set; }    
     }
 }
--- aspnet-core/src/toyiyo.todo.Application/Subscriptions/Dto/SubscriptionDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Subscriptions/Dto/SubscriptionDto.cs
@@ -3,22 +3,83 @@ using Abp.AutoMapper;
 using Stripe;
 namespace toyiyo.todo.application.subscriptions
 {
+    /// <summary>
+    /// Represents a subscription for the tenant.
+    /// </summary>
+    /// <summary>
+    /// Represents a data transfer object for a subscription.
+    /// </summary>
     [AutoMap(typeof(Subscription))]
     public class SubscriptionDto
     {
+        /// <summary>
+        /// Gets or sets the ID of the subscription.
+        /// </summary>
         public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the subscription is active.
+        /// </summary>
         public bool Active { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount of the subscription.
+        /// </summary>
         public int Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount of the subscription in decimal format.
+        /// </summary>
         public string AmountDecimal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the billing scheme of the subscription.
+        /// </summary>
         public string BillingScheme { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date of the subscription.
+        /// </summary>
         public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interval count of the subscription.
+        /// </summary>
         public int IntervalCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the subscription is in live mode.
+        /// </summary>
         public bool LiveMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the nickname of the subscription.
+        /// </summary>
         public string Nickname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the product associated with the subscription.
+        /// </summary>
         public string ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the plan associated with the subscription.
+        /// </summary>
         public string PlanId { get; set; }
-        public string CustomerId {get; set;}
+
+        /// <summary>
+        /// Gets or sets the ID of the customer associated with the subscription.
+        /// </summary>
+        public string CustomerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the portal session associated with the subscription.
+        /// </summary>
         public string PortalSessionUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the product associated with the subscription.
+        /// </summary>
         public string ProductName { get; set; }
     }
 }
