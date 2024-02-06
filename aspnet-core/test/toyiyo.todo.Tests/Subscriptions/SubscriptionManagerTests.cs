1348b37c4a12c000ea3b7f802adcf1d0607a217e
Jose M Delgado <jdelgado2002@gmail.com>
2024-02-05 20:24:27 -0600

    Matching seats to active users.

--- aspnet-core/test/toyiyo.todo.Tests/Subscriptions/SubscriptionManagerTests.cs
+++ aspnet-core/test/toyiyo.todo.Tests/Subscriptions/SubscriptionManagerTests.cs
@@ -19,54 +19,5 @@ namespace toyiyo.todo.Tests.Subscriptions
             _subscriptionManager = Resolve<SubscriptionManager>();
         }
 
-        [Fact]
-        public void GetSubscriptionCustomerByEmail_ValidEmail_ReturnsCustomer()
-        {
-            // Arrange
-            var email = "jdelgado@toyiyo.com";
-
-            // Act
-            var result = _subscriptionManager.GetSubscriptionCustomerByEmail(email);
-
-            // Assert
-            result.ShouldNotBeNull();
-            result.Email.ShouldBe(email);
-        }
-
-        [Fact]
-        public void GetSubscriptionCustomerByEmail_InvalidEmail_ThrowsException()
-        {
-            // Arrange
-            var email = "invalid@example.com";
-
-            // Act & Assert
-            Should.Throw<UserFriendlyException>(() => _subscriptionManager.GetSubscriptionCustomerByEmail(email));
-        }
-
-        [Fact]
-        public void GetSubscriptionByEmail_ValidEmail_ReturnsSubscription()
-        {
-            // Arrange
-            var email = "jdelgado@toyiyo.com";
-
-            // Act
-            var result = _subscriptionManager.GetSubscriptionByEmail(email);
-
-            // Assert
-            result.ShouldNotBeNull();
-            result.Data.ShouldNotBeNull();
-            result.Data.First().Status.ShouldBe("active");
-        }
-
-        [Fact]
-        public void GetSubscriptionByEmail_InvalidEmail_ThrowsException()
-        {
-            // Arrange
-            var email = "invalid@example.com";
-
-            // Act & Assert
-            Should.Throw<UserFriendlyException>(() => _subscriptionManager.GetSubscriptionByEmail(email));
-        }
-
     }
 }
