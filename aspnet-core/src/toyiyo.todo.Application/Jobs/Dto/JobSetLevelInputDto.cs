b22a875bd14c991a32e67b5ffbdfe1c27d4d787e
Jose M Delgado <jdelgado2002@gmail.com>
2024-11-13 11:50:39 -0600

    enhance Job DTOs with detailed XML documentation; update job editing modal to dynamically populate job levels

--- aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetLevelInputDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetLevelInputDto.cs
@@ -1,14 +1,28 @@
-
 using System;
 using System.ComponentModel.DataAnnotations;
 using static toyiyo.todo.Jobs.Job;
 
+/// <summary>
+/// Data Transfer Object for setting the level of a job.
+/// </summary>
+/// <remarks>
+/// This DTO is used to update the level of an existing job by providing the job's ID and the new level.
+/// </remarks>
 namespace toyiyo.todo.Jobs.Dto
 {
+    /// <summary>
+    /// Data Transfer Object for updating the level of a job.
+    /// </summary>
     public class JobSetLevelInputDto
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the job to update.
+        /// </summary>
         [Required]
         public Guid Id { get; set; }
+        /// <summary>
+        /// Gets or sets the new level to be assigned to the job.
+        /// </summary>
         [Required]
         [EnumDataType(typeof(JobLevel))]
         public JobLevel Level { get; set; }
--- aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobUpdateInputDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobUpdateInputDto.cs
@@ -1,14 +1,49 @@
 
+using System.ComponentModel.DataAnnotations;
 using static toyiyo.todo.Jobs.Job;
 
 namespace toyiyo.todo.Jobs.Dto
 {
+    /// <summary>
+    /// Data transfer object for updating a job entity.
+    /// </summary>
+    /// <remarks>
+    /// This DTO contains all the necessary fields to update an existing job in the system.
+    /// </remarks>
+    /// <seealso cref="Job"/>
     public class JobUpdateInputDto
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the job.
+        /// </summary>
+        [Required]
         public System.Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the job.
+        /// Must not exceed the maximum length defined in Job.MaxTitleLength.
+        /// </summary>
+        [Required]
+        [StringLength(Job.MaxTitleLength)]
         public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the job.
+        /// Must not exceed the maximum length defined in Job.MaxDescriptionLength.
+        /// </summary>
+        [StringLength(Job.MaxDescriptionLength)]
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the due date of the job.
+        /// Can be null if no due date is set.
+        /// </summary>
         public System.DateTime? DueDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the importance level of the job.
+        /// </summary>
+        /// [required]
         public JobLevel Level { get; set; }
     }
 }
