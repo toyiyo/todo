756e14694cd45f7ba90b563e8dfaf44492a2150d
Jose M Delgado <jdelgado2002@gmail.com>
2024-11-13 09:14:19 -0600

    validating set level dto

--- aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetLevelInputDto.cs
+++ aspnet-core/src/toyiyo.todo.Application/Jobs/Dto/JobSetLevelInputDto.cs
@@ -1,12 +1,16 @@
 
 using System;
+using System.ComponentModel.DataAnnotations;
 using static toyiyo.todo.Jobs.Job;
 
 namespace toyiyo.todo.Jobs.Dto
 {
     public class JobSetLevelInputDto
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
+        [EnumDataType(typeof(JobLevel))]
         public JobLevel Level { get; set; }
     }
 }
