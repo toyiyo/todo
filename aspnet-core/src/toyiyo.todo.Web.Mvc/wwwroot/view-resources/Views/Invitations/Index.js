c2f39fc0abf17d367c699526e979394c7d99a56c
Jose M Delgado <jdelgado2002@gmail.com>
2024-11-29 19:04:56 -0600

    displaying existing invites

---
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/wwwroot/view-resources/Views/Invitations/Index.js
@@ -0,0 +1,45 @@
+(function ($) {
+  const _invitationService = abp.services.app.userInvitation;
+  const l = abp.localization.getSource('todo');
+  const _$table = $('#InvitationsTable');
+
+  const invitationsTable = _$table.DataTable({
+    paging: true,
+    serverSide: true,
+    select: true,
+    listAction: {
+      ajaxFunction: _invitationService.getAll,
+    },
+    buttons: [],
+    columnDefs: [
+      {
+        targets: 0,
+        data: 'email',
+        className: "email",
+      },
+      {
+        targets: 1,
+        data: 'expirationDate',
+        defaultContent: '',
+        render: (data, type, row, meta) => {
+            if (!data) {
+                return 'never';
+            }
+            const expirationDate = moment(data).format('LLL');
+            return `<span title="expires on ${expirationDate}">${expirationDate}</span>`;
+        }
+      },
+      {
+        targets: 2,
+        data: "acceptedDate",
+        render: (data, type, row, meta) => {
+            if (!data) {
+                return l('NotAccepted');
+            }
+            const acceptedDate = moment(data).format('LLL');
+            return `<span title="accepted on ${acceptedDate}">${acceptedDate}</span>`;
+        }
+      }
+    ],
+  });
+})(jQuery);
