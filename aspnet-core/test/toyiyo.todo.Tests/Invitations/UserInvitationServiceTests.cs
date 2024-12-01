cbbe52ade91f79bdd38b4cbd6806cfeb61e4b4fe
Jose M Delgado <jdelgado2002@gmail.com>
2024-12-01 11:59:09 -0600

    mocking email service

--- aspnet-core/test/toyiyo.todo.Tests/Invitations/UserInvitationServiceTests.cs
+++ aspnet-core/test/toyiyo.todo.Tests/Invitations/UserInvitationServiceTests.cs
@@ -8,6 +8,7 @@ using toyiyo.todo.Invitations;
 using toyiyo.todo.Invitations.Dto;
 using System;
 using System.Collections.Generic;
+using Abp.Net.Mail;
 
 namespace toyiyo.todo.Tests.Invitations
 {
@@ -15,12 +16,14 @@ namespace toyiyo.todo.Tests.Invitations
     {
         private readonly IUserInvitationAppService _userInvitationService;
         private readonly TenantManager _tenantManager;
+        private readonly IEmailSender _emailSender;
 
         public UserInvitationServiceTests()
         {
             // Register IUserInvitationService
             _userInvitationService = Resolve<IUserInvitationAppService>();
             _tenantManager = Resolve<TenantManager>();
+            _emailSender = Resolve<IEmailSender>();
             // Call LoginAsDefaultTenantAdmin before each test
             LoginAsDefaultTenantAdmin();
         }
