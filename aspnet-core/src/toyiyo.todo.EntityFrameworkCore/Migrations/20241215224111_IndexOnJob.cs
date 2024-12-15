70f82eb096ae0396980ca07c96ed92f520bcd67a
Jose M Delgado <jdelgado2002@gmail.com>
2024-12-15 17:08:02 -0600

    Add TenantId to indexes for Jobs and Projects, and update migration for new indexes

---
+++ aspnet-core/src/toyiyo.todo.EntityFrameworkCore/Migrations/20241215224111_IndexOnJob.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace toyiyo.todo.Migrations
+{
+    public partial class IndexOnJob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_TenantId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_JobStatus_Level",
+                table: "Jobs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_TenantId_Id_IsDeleted",
+                table: "Projects",
+                columns: new[] { "TenantId", "Id", "IsDeleted" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_JobStatus_Level_TenantId",
+                table: "Jobs",
+                columns: new[] { "JobStatus", "Level", "TenantId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_TenantId_Id_IsDeleted",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_JobStatus_Level_TenantId",
+                table: "Jobs");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_TenantId",
+                table: "Projects",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_JobStatus_Level",
+                table: "Jobs",
+                columns: new[] { "JobStatus", "Level" });
+        }
+    }
+}
