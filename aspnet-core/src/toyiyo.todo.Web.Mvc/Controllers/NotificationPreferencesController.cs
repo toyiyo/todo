20ea3a5453935e2926f8bb783b068dd90d0cfc7e
Jose M Delgado <jdelgado2002@gmail.com>
2025-02-24 11:03:11 -0600

    Add authorization attribute to NotificationPreferencesController

--- aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/NotificationPreferencesController.cs
+++ aspnet-core/src/toyiyo.todo.Web.Mvc/Controllers/NotificationPreferencesController.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using toyiyo.todo.Controllers;
 using toyiyo.todo.Notifications;
 
 namespace toyiyo.todo.Web.Mvc.Controllers
 {
+    [AbpMvcAuthorize]
     public class NotificationPreferencesController : todoControllerBase
     {
         private readonly INotificationPreferenceAppService _notificationPreferenceAppService;
